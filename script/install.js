351218feffe5df5a113a78cbcb860391264250f8
荣顶 <wangrongding@qq.com>
2021-11-01 11:23:34 +0800

    fix: 修复遗漏安装main项目依赖的问题

--- script/install.js
+++ script/install.js
@@ -5,11 +5,12 @@ const appList = require("./appList");
 const exec = util.promisify(require("child_process").exec);
 
 /**
-* @Description 安装所有目标项目的依赖
-* @Param {ObjectType} targetPath : packages中的项目名称
-* @Param {ObjectType} installMethod : 项目依赖安装方式
-*/
+ * @Description 安装所有目标项目的依赖
+ * @Param {ObjectType} targetPath : packages中的项目名称
+ * @Param {ObjectType} installMethod : 项目依赖安装方式
+ */
 async function install(targetPath, installMethod) {
+	const load = loading(`请耐心等待,正在安装依赖...`).start();
 	let command = "";
 	switch (installMethod) {
 		case "yarn":
@@ -30,8 +31,12 @@ async function install(targetPath, installMethod) {
 			break;
 	}
 	const { stdout, stderr } = await exec(command, {
-		cwd: path.resolve(process.cwd(), "packages/" + targetPath),
+		cwd: path.resolve(
+			process.cwd(),
+			targetPath ? "main" : "packages/" + targetPath
+		),
 	});
+	load.stop();
 	console.log(targetPath, stdout, "✅");
 	stderr && console.error(targetPath, "❓❓", stderr);
 }
@@ -45,10 +50,9 @@ console.log(
 );
 
 appList.forEach(async (item, index) => {
-	const load = loading(`请耐心等待,${item.repoName}>正在安装依赖...`).start();
 	await install(item.repoName, item.installMethod);
-	load.stop();
 });
+install("", "yarn");
 
 process.on("unhandledRejection", (reason, p) => {
 	console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
